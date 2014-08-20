0e65b3220edf70470839a00c912ac612c9e2d8ab
Abrer <ahbrer@gmail.com>
2014-08-20 19:46:39 -0400

    First commit

---
+++ submath.py
@@ -0,0 +1,241 @@
+__author__ = 'abrer'    # PyCharm adds this to everything....
+# Last updated 08 / 20 / 2014
+# Yet Another Subnet Calculator --  YASC -- submath
+
+'''
+    - Review GetWorkingOctet* functions
+    - Fix line 113 and other things
+'''
+
+import sys
+
+def cidr_to_decimal(network_bits):
+    # Take CIDR /notation (/30) and return the decimal equivalent
+
+    network_bits = network_bits[1:]  # Remove the / at the beginning
+
+    full_octets = int(network_bits) / 8
+    octets_left = 4 - int(full_octets)
+    remaining_bits = int(network_bits) % 8
+    remaining_bits = str(remaining_bits)
+
+    bit_decimal_vals = {
+        '0':   '0.',
+        '1': '128.',
+        '2': '192.',
+        '3': '224.',
+        '4': '240.',
+        '5': '248.',
+        '6': '252.',
+        '7': '254.',
+        '8': '255.'
+    }
+
+
+    decimal_mask = ''
+
+    for i in range(0, full_octets):
+        decimal_mask += '255.'
+        if i == full_octets - 1:
+            octets_left -= 1
+            if int(remaining_bits) > 0:
+                decimal_mask += bit_decimal_vals[remaining_bits]
+
+    # Add 0's to remaining octets
+    for i in range(0, octets_left):
+        if octets_left == 0:
+            break
+        else:
+            decimal_mask += '0.'
+
+    return decimal_mask[:-1]
+
+def get_first_usable_address(network_address):
+    #Get first usable address
+    network_address = network_address.split('.')
+
+    # String to hold final address
+    first_usable = ''
+
+    # Turns out the first usable address is just the last octet + 1
+    for i in range(0, len(network_address)):
+        if i == 3:
+            network_address[i] = str(int(network_address[i]) + 1)
+        # Just adding decimal points so our address looks proper.
+        first_usable += network_address[i] + '.'
+
+    return first_usable[:-1]
+
+def get_last_usable_address(broadcast_address):
+    # Just -1 from the last octet apparently?
+    # let's try it...
+    broadcast_address = broadcast_address.split('.')
+
+    # Last octet - 1
+    for i in range(0, len(broadcast_address)):
+        if i == 3:
+            broadcast_address[i] = str(int(broadcast_address[i]) - 1)
+
+    # String to hold our final address
+    last_usable = ''
+
+    # Just adding decimal points so our address looks proper.
+    for i in range(0, len(broadcast_address)):
+        last_usable += broadcast_address[i] + '.'
+
+    #Shaves off the last . from the string so it looks nice. Slices are awesome!
+    return last_usable[:-1]
+
+def get_broadcast(ip_net_address, working_octet, net_increment):
+    # Currently returns Broadcast
+    net_address = ip_net_address.split('.')
+    working_octet -= 1
+
+    for i in range(0, len(net_address)):
+        if i == working_octet:
+            net_address[i] = str(int(net_address[i]) + net_increment -1)
+        if i > working_octet:
+            net_address[i] = '255'
+
+    broadcast_address = ''
+
+    for i in range(0, len(net_address)):
+        broadcast_address += net_address[i] + '.'
+
+    return broadcast_address[:-1]
+
+def get_network_address(ip_address, working_octet, net_increment):
+    # Get Network Address
+    working_octet -= 1  # Subtract 1 since list index start at 0.
+
+    ip_address = ip_address.split('.')
+
+    # For every octet past initial work octet, set to 0
+    for i in range(0, len(ip_address)):
+        if i > working_octet:
+            ip_address[i] = '0'
+
+    # Math for network address
+    ip_address[working_octet] = str((int(ip_address[working_octet]) / int(net_increment)) * net_increment)
+
+    network_address = ip_address[0] + '.' + ip_address[1] + '.' + ip_address[2]\
+                      + '.' + ip_address[3]  # Find a better way to do this
+
+    return network_address
+
+def get_working_octet_value(subnet_mask):
+    # Return the DECIMAL VALUE of working octet
+    # CLONE OF GETWORKINGOCTET() til I figure out if I want to combine them.
+    subnet_mask = subnet_mask.split('.')
+    working_octet_value = ''
+
+    for i in range(0, len(subnet_mask)):
+        if subnet_mask[i] != '255':
+            working_octet_value = subnet_mask[i]
+            break
+        elif i == 3: # DEBUGGING PURPOSES -- in the event that mask is /32
+            working_octet_value = subnet_mask[i]
+
+    return working_octet_value
+
+def get_binary_mask(subnet_mask):
+    # Return Mask in Binary
+    subnet_mask = subnet_mask.split('.')
+    binary_mask= []
+    binary_mask_string = ''
+
+    for i in range(0, len(subnet_mask)):
+        # Convert each octet to binary
+        binary_mask.append(format(int(subnet_mask[i]), '08b'))
+
+        # And save to MaskString
+        binary_mask_string = binary_mask_string + binary_mask[i] + '.'
+
+
+    return binary_mask_string[:-1]  # Remove the '.' at the end of the string.
+
+def get_network_bits(subnet_mask):
+    # Based on Subnet Mask
+    bits = 0
+
+    # Decimal:          128 192 224 240 248 252 254 255
+    # Bits in Mask:     1   2    3   4  5   6   7   8
+
+    # Dictionary of subnet vals
+    # and their # of bits in an octet
+    mask_bit_values = {
+        '0':   0,
+        '128': 1,
+        '192': 2,
+        '224': 3,
+        '240': 4,
+        '248': 5,
+        '252': 6,
+        '254': 7,
+        '255': 8
+    }
+
+    subnet_mask = subnet_mask.split('.')
+
+    for octet in subnet_mask:
+        if octet == '255':
+            bits += 8
+        if octet != '255':
+            # Do math to get bits from value and add to bits var
+            if octet in mask_bit_values:
+                bits += mask_bit_values[octet]
+    return bits
+
+def get_working_octet(subnet_mask):
+    # Return the octet in which we can work
+    subnet_mask = subnet_mask.split('.')
+    working_octet = ''
+
+    for i in range(0, len(subnet_mask)):
+        if subnet_mask[i] != '255':
+            working_octet = i+1
+            break
+        else:
+            working_octet = 4  # This is in the case that the mask is a /32 for now. DEBUGGING PURPOSES.
+
+    return working_octet
+
+def get_net_increment(working_octet_value):
+    # Return the Network Increment based on
+    # the working octet value of our Subnet Mask.
+    if working_octet_value == '0':
+        return 1
+    else:
+        return 256 - int(working_octet_value)
+
+def get_class():
+    # Get our class!
+    print 'Work in progress'
+
+def err_check_input(arguments):
+
+    #[0] should be script location
+    #[1] should be IP Address
+    #[2] Should be Subnet in DECIMAL or CIDR notation
+
+    if len(arguments) < 3:
+        print '\nOh dear! You need 2 arguments! '
+        print 'Something like: $ yasc.py 192.168.2.25 255.255.255.0'
+        print 'OR: $ yasc.py 192.168.2.25 /24\n'
+        sys.exit(0)
+
+    elif len(arguments) > 3:
+        print '\nNow that\'s just too damn many son. You only need TWO arguments.'
+        print 'Something like: $ yasc.py 192.168.2.25 255.255.255.0'
+        print 'OR: $ yasc.py 192.168.2.25 /24\n'
+        sys.exit(0)
+
+    if '/' in arguments[2]:
+        bits = arguments[2]
+        bits = int(bits[1:])
+        if bits > 30:
+            print '\nDem bits is too damn high! Don\'t exceed /30!'
+            print 'Should I have a use for that in the future I\'ll add it in.\n'
+            sys.exit(0)
+    else:   # This else doesn't actually do anything. lol
+        pass
---
+++ yasc.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python2
+__author__ = 'abrer'    # PyCharm adds this to everything.... testbranch1
+# Last updated 08 / 20 / 2014
+# Yet Another Subnet Calculator --  YASC 
+
+""" Last working on:
+
+        - Moved calc functions to their own file: submath.py
+        - Those functions are now imported.
+
+    TODO:
+        - Fix input of masks at /8 or lower:
+            /8 shows 255.0.0
+            Lower shows 0.0.0.0
+        - Finish the GetClass() function! It's easy, dunno why I haven't bothered yet.
+        - Fix line 92 -- use a for loop to add . for readability... mehbe.
+        - Add subnets per host and etc...
+        - Colorize output for better visibility -- hopefully with ANSI escape codes for $BASH.
+"""
+import sys
+
+# import all of the old functions now in the
+# newly created submath.py file!
+from submath import *
+
+def main():
+
+    # # Fixed values for debugging
+    # ip_address = '192.168.15.43'
+    # # subnet_mask = '255.255.255.252'
+
+    err_check_input(sys.argv)
+
+    # Detect CIDR (/20, /24, /29) notation.
+    if '/' in sys.argv[2]:
+        # If CIDR ('/'), convert to decimal
+        subnet_mask = cidr_to_decimal(sys.argv[2])
+    else:
+        # If no '/' found, it's already in decimal
+        subnet_mask = sys.argv[2]
+    ip_address = sys.argv[1]
+
+    # Some handy vars!
+    network_bits = get_network_bits(subnet_mask)
+    working_octet = get_working_octet(subnet_mask)
+    working_octet_value = get_working_octet_value(subnet_mask)
+    
+    net_increment = get_net_increment(working_octet_value)
+    binary_mask = get_binary_mask(subnet_mask)
+
+    network_address = get_network_address(ip_address,
+                                          working_octet, net_increment)
+    
+    broadcast_address = get_broadcast(network_address,
+                                      working_octet, net_increment)
+
+    last_usable_address = get_last_usable_address(broadcast_address)
+    first_usable_address = get_first_usable_address(network_address)
+
+    # Print our info to the screen!
+    print ''
+    print 'IP Address: \t%s /%s'        % (ip_address, network_bits)
+    print 'Subnet Mask: \t%s'           % subnet_mask
+    print '-' * 40                      # Divider!
+    print 'Network Addr: \t%s'          % network_address
+    print 'Network Range: \t%s - %s'    % (first_usable_address, last_usable_address)
+    print 'Broadcast: \t%s'             % broadcast_address
+    print '-' * 40                      # Divider!
+    print 'Binary Mask: \t%s'           % binary_mask
+    print 'Net Increment: \t%s'         % net_increment
+    print ''
+
+
+
+    # These values are used for calculating subnet information
+    # and aren't generally relevant to the subnetter.
+    #
+    # Working octet may be useful as it's the relevant octet
+    # for which we are subnetting.
+
+    # print 'Working Octet: \t\t%s'     % working_octet
+    # print 'Working Octet Val: \t%s'   % working_octet_value
+
+
+# Boilerplate to start main()
+if __name__ == '__main__':
+    main()
